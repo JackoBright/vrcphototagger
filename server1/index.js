a78b3f22bfe4cf71197e54c3746baf7ddb31318c
Michael Molineaux <thepitcher23@gmail.com>
2025-04-20 17:10:51 +0000

    Yes

---
+++ server1/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+const bodyParser = require('body-parser')
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now()+ '-' + file.originalname);
+
+    }
+})
+const upload = multer({storage: storage});
+
+
+
+app.get('/', (req,res) => res.sendFile('/webserver/server1/frontpage.html'));
+app.get('/stylesheet.css', (req,res)=> res.sendFile('/webserver/server1/stylesheet.css'))
+app.get('/uploads', (req, res, err) => {
+    res.status(200).send("Gay Man");
+
+})
+app.get('/profile.html', (req, res, err) => {
+    res.status(200).send("Gay Man Tinder");
+
+})
+
+app.post('/upload', upload.array('image'), (req,res,err) => {
+    console.log(req.file)
+    if(err){
+        console.log(err)
+    }
+    res.status(200).redirect('/');
+
+    
+})
+
+app.listen(port, () => console.log(`Server listening at http://localhost:${port}`))
